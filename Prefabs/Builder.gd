f00e478de1ee84877609ff0174877405964b21e8
Peter Kiss <peter@yscik.com>
2018-03-27 02:02:04 +0200

    builder add more tiles button

--- Prefabs/Builder.gd
+++ Prefabs/Builder.gd
@@ -3,6 +3,7 @@ extends Node2D
 signal built
 
 var moved_block
+var tiles_scene = preload("res://Levels/Workshop/BuilderTiles.tscn")
 
 onready var current_pos = global_position
 
@@ -10,54 +11,51 @@ func _ready():
 	
 	pass
 
-func tween(method, from, to):
-	$Tween.interpolate_method(self, method, from, to, 1, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
+func move_to(pos):
+	$Tween.interpolate_method(self, "set_pos", current_pos, pos, 1, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
 	$Tween.start()
 	
 var i = 0
+
 func build_block(pos):
 	
-	if $Tiles.get_child_count() <= i: return
+	if $Tiles.get_child_count() <= i: 
+		click_add_tiles(pos)
+		return
 	
 	var block = $Tiles.get_child(i)
 	i += 1
 	
 	if(!block): 
-		#tween("move_to", current_pos, global_position)
 		return
 	
-	print("starting %s" % block.name)
-
-	block.raise()
-		
-	tween("move_to", current_pos, block.global_position)
+	move_to(block.global_position)
 	yield($Tween, "tween_completed")
 
+	block.set_collision_mask_bit(4, false)
+	if State.player.grab.grabbed == block:
+		State.player.grab.drop()
+		
 	moved_block = block
 	update_moved_block()
 
-	print("picked up %s" % block.name)
-
-	tween("move_to", current_pos, pos)	
+	move_to(pos)	
 	yield($Tween, "tween_completed")
 	
 	moved_block = null
 
 	print("built %s" % block.name)
 	block.visible = false
-	#block.queue_free()
 	emit_signal("built")
 	
 	pass
 
-func move_to(pos):
+func set_pos(pos):
 	
 	if !$Cursor.working:
 		$Tween.stop(self)
 		moved_block = null
-#	$ArmRot.look_at(pos)
-#	var length = (global_position - pos).length()
-#	$ArmRot/Arm.rect_size.y = length
+		
 	current_pos = pos
 	$Cursor.global_position = pos
 	update_moved_block()
@@ -66,4 +64,20 @@ func update_moved_block():
 	if(moved_block): 
 		moved_block.global_position = current_pos
 
+func click_add_tiles(pos):
+
+	var tiles_node = $Tiles
+	remove_child(tiles_node)
+	tiles_node.queue_free()
+	
+	move_to($ToolbarUi/Button.global_position)
+	yield($Tween, "tween_completed")
+	
+	var tiles = tiles_scene.instance()
+	add_child(tiles)
+	i = 0
+	build_block(pos)
+	
+	pass
+	
 	
