33068414a8b4e2cf0fd858b99942d1ee7b2bfe4f
Peter Kiss <peter@yscik.com>
2018-03-27 18:58:48 +0200

    fix tile positioning, allow them to be used across levels

--- Prefabs/MovableTile.gd
+++ Prefabs/MovableTile.gd
@@ -6,28 +6,42 @@ export var hideSprite = true
 export var on = true
 var content = null
 
+var tilemap
+
 func _ready():
 	if(on): on()
 
 func off():
+	grabbed = true
 	if content != null: 
 		change_map(content)
 	if hideSprite:
 		$Sprite.visible = true
-	
+		
+	get_parent().remove_child(self)
+	State.player.grab.add_child(self)
+
 func on():
 	content = change_map(tile)
+	grabbed = false
 	if hideSprite:
 		$Sprite.visible = false
+
 	
 func change_map(value):
 	
-	var tilemap = get_map()
+	tilemap = get_map()
+	
+	if !tilemap: return
 	
 	var pos = tilemap.world_to_map(tilemap.to_local(global_position))
 	var prev = tilemap.get_cellv(pos)
 	
-	global_position = tilemap.map_to_world(pos) + tilemap.global_position + OFFSET
+	if grabbed:
+		get_parent().remove_child(self)
+		tilemap.get_parent().add_child(self)
+
+	global_position = tilemap.to_global(tilemap.map_to_world(pos) + OFFSET)
 	
 	tilemap.set_cellv(pos, value)
 	tilemap.update_bitmask_area(pos)
@@ -35,7 +49,8 @@ func change_map(value):
 	return prev
 	
 func get_map():
-	var node = self
+		
+	var node = State.area if State.area else self
 	while(node):
 		var map = node.find_node("TileMap")
 		if map:
--- platformer/EnterKey.gd
+++ platformer/EnterKey.gd
@@ -1,18 +1,4 @@
-extends Sprite
-
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+extends CollisionObject2D
 
 
 func click():
@@ -20,7 +6,8 @@ func click():
 	$Tween.interpolate_property(self, "rotation", rotation, rotation + deg2rad(90), 0.5, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
 	$Tween.start()
 
-func _gui_input(event):
+func _input_event(viewport, event, shape_idx):
+	print("pi")
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
 		click()
 
